a1da5df9681a3b67b32f535c1d02bcce7df8caca
Pranay Chandrikapure <116444922+Pranaychandrikapure@users.noreply.github.com>
2025-02-04 01:57:16 +0530

    Update settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -1,12 +1,9 @@
 from pathlib import Path
-import os
 import dj_database_url
 # from django.conf import settings
 # import razorpay
 
-# client = razorpay.Client(auth=(settings.RAZORPAY_API_KEY, settings.RAZORPAY_API_SECRET))
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# client = razorpay.Client(auth=(settings.RAZORPAY_API_KEY, settings.RAZORPAY_API_SECRET
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,13 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static/'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
